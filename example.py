78627a3b3cdde92b4db9df1cd3158fab2e42cf8f
Andreas Piotrowski <56364594+bookofproofs@users.noreply.github.com>
2021-11-07 13:21:30 +0100

    override rule derivations with placeholders, README extended.

--- example.py
+++ example.py
@@ -7,19 +7,20 @@ path_to_grammar = "../TatSuRDG/tests/tatsu.ebnf"  # path to your tatsu ebnf gram
 
 class ExampleRandomDerivation:
 
-    def __init__(self, grammar_name):
+    def __init__(self, grammar_name, override_placeholders: dict):
         path_to_grammar = "tests/" + grammar_name
         with io.open(path_to_grammar, 'r', encoding="utf-8") as file:
             grammar = file.read()
             self.parser = tatsu.compile(grammar)
 
-        self.rg = TatSuRandomDerivation(self.parser, max_length_regex=5, max_counter=5, recursion_limit=1500)
+        self.rg = TatSuRandomDerivation(self.parser, max_length_regex=5, max_counter=5, recursion_limit=1500,
+                                        override_placeholders=override_placeholders)
 
     def derive(self, rule_name):
         return self.rg.random_derivation(rule_name)
 
 
-ex = ExampleRandomDerivation("calc.ebnf")
+ex = ExampleRandomDerivation("calc.ebnf", {"number": "1"})
 for i in range(1, 50):
     ex.rg.init_rules()
     print(ex.derive("expression"))
